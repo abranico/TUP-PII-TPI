dd5a4a0ec7f0208101f8fb5ca44698af194ba1ef
abranico <abranico011@gmail.com>
2023-09-03 19:28:02 -0300

    correccion de gestionar prestamo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Trabajo Práctico I - Programación II
 
-import bibloteca as b
+import bibloteca
 import os
 
 print("Bienvenido!")
@@ -12,42 +12,8 @@ def menu():
     print("3 - Registrar nuevo libro")
     print("4 - Eliminar ejemplar")
     print("5 - Mostrar ejemplares prestados")
-    print("6 - Salir")
-    
-def gestionar_prestamo():
-    codigo_libro = input("ingrese el codigo del libro: ")
-    indice_libro = int()
-    encontrado = False
-
-    for i,libro in enumerate(b.libros):
-        if libro.get('cod') == codigo_libro:
-            indice_libro = i
-            encontrado = True
-            break
-        
-    if encontrado == True:
-        titulo = b.libros[indice_libro]['titulo']
-        autor = b.libros[indice_libro]['autor']
-        ejemplares_disponibles = b.libros[indice_libro]['cant_ej_ad'] - b.libros[indice_libro]['cant_ej_pr']
-        print(f"""
-              Titulo: {titulo }
-              Autor: {autor}
-              Ejemplares disponibles: {ejemplares_disponibles}
-              """)
-        if ejemplares_disponibles > 0:
-           confirmar_prestamo = input("¿Desea confirmar el prestamo de este libro? (Si/No) ")
-           confirmar_prestamo = confirmar_prestamo.upper()
-           if confirmar_prestamo == "SI":
-               b.libros[indice_libro]['cant_ej_pr'] += 1
-               print("Prestamo realizado con exito.")
-           else:
-               print("Prestamo cancelado.")
-        else:
-            print("No quedan mas ejemplares disponibles.")  
-    else:
-        print("Libro no encontrado")
-            
-           
+    print("6 - Salir") 
+                    
 
 while respuesta != "salir":
     menu()
@@ -55,7 +21,7 @@ while respuesta != "salir":
     os.system ("cls") #Limpiar pantalla
     if opt.isnumeric():
         if int(opt) == 1:
-            gestionar_prestamo()
+            bibloteca.prestar_ejemplar_libro()
             print()
         elif int(opt) == 2:
             #completar
--- bibloteca.py
+++ bibloteca.py
@@ -22,8 +22,37 @@ def eliminar_ejemplar_libro():
     return None
 
 def prestar_ejemplar_libro():
-    #completar
-    return None
+    codigo_libro = input("ingrese el codigo del libro: ")
+    indice_libro = int()
+    encontrado = False
+
+    for i,libro in enumerate(libros):
+        if libro.get('cod') == codigo_libro:
+            indice_libro = i
+            encontrado = True
+            break
+        
+    if encontrado == True:
+        titulo = libros[indice_libro]['titulo']
+        autor = libros[indice_libro]['autor']
+        ejemplares_disponibles = libros[indice_libro]['cant_ej_ad'] - libros[indice_libro]['cant_ej_pr']
+        print(f"""
+              Titulo: {titulo }
+              Autor: {autor}
+              Ejemplares disponibles: {ejemplares_disponibles}
+              """)
+        if ejemplares_disponibles > 0:
+           confirmar_prestamo = input("¿Desea confirmar el prestamo de este libro? (Si/No) ")
+           confirmar_prestamo = confirmar_prestamo.upper()
+           if confirmar_prestamo == "SI":
+               libros[indice_libro]['cant_ej_pr'] += 1
+               print("Prestamo realizado con exito.")
+           else:
+               print("Prestamo cancelado.")
+        else:
+            print("No quedan mas ejemplares disponibles.")  
+    else:
+        print("Libro no encontrado")
 
 def devolver_ejemplar_libro():
     #completar
